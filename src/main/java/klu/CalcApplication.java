2e850b3f138f9b193b4ed91c890fc071cb05057a
AshokSidhid <152054898+AshokSidhid@users.noreply.github.com>
2025-08-26 12:44:22 +0530

    Update CalcApplication.java

--- src/main/java/klu/CalcApplication.java
+++ src/main/java/klu/CalcApplication.java
@@ -2,13 +2,19 @@ package klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CalcApplication {
+public class CalcApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalcApplication.class, args);
-		System.out.print("Running...");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CalcApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(CalcApplication.class, args);
+        System.out.println("Running...");
+    }
 }
